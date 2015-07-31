cd5a778c1fa78f345ffda4cff68b5fc6b5a74cba
Evancli <evancli@gmail.com>
2015-07-30 23:28:26 -0700

    Implemented BLE device selection.

--- Android/JanBot/app/src/main/java/com/evanli/janbot/MainActivity.java
+++ Android/JanBot/app/src/main/java/com/evanli/janbot/MainActivity.java
@@ -1,18 +1,134 @@
 package com.evanli.janbot;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothGattCharacteristic;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.ServiceConnection;
 import android.os.Bundle;
+import android.os.IBinder;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import de.greenrobot.event.EventBus;
+
 public class MainActivity extends Activity {
+    private final static String TAG = "JanBot";
+
+    public static final String EXTRAS_DEVICE_NAME = "DEVICE_NAME";
+    public static final String EXTRAS_DEVICE_ADDRESS = "DEVICE_ADDRESS";
+
+    private BluetoothLeService mBluetoothLeService;
+    private boolean mConnected = false;
+    private BluetoothGattCharacteristic mNotifyCharacteristic;
+    private String mDeviceName;
+    private String mDeviceAddress;
+
+    // Events
+
+    public class DeviceStatusEvent {
+        public final String statusMessage;
+
+        public DeviceStatusEvent(String statusMessage) {
+            this.statusMessage = statusMessage;
+        }
+    }
+    public class DeviceSelectedEvent {
+        public final String deviceName;
+        public final String deviceAddress;
+
+        public DeviceSelectedEvent(String deviceName, String deviceAddress) {
+            this.deviceName = deviceName;
+            this.deviceAddress = deviceAddress;
+        }
+    }
+
+    // Code to manage Service lifecycle.
+    private final ServiceConnection mServiceConnection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder service) {
+            mBluetoothLeService = ((BluetoothLeService.LocalBinder) service).getService();
+            if (!mBluetoothLeService.initialize()) {
+                Log.e(TAG, "Unable to initialize Bluetooth");
+                finish();
+            }
+            // Automatically connects to the device upon successful start-up initialization.
+            mBluetoothLeService.connect(mDeviceAddress);
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            mBluetoothLeService = null;
+        }
+    };
+
+    // Handles various events fired by the Service.
+    // ACTION_GATT_CONNECTED: connected to a GATT server.
+    // ACTION_GATT_DISCONNECTED: disconnected from a GATT server.
+    // ACTION_GATT_SERVICES_DISCOVERED: discovered GATT services.
+    // ACTION_DATA_AVAILABLE: received data from the device.  This can be a result of read
+    //                        or notification operations.
+    private final BroadcastReceiver mGattUpdateReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (BluetoothLeService.ACTION_GATT_CONNECTED.equals(action)) {
+                mConnected = true;
+                updateConnectionState(R.string.connected);
+//                invalidateOptionsMenu();
+            } else if (BluetoothLeService.ACTION_GATT_DISCONNECTED.equals(action)) {
+                mConnected = false;
+                updateConnectionState(R.string.disconnected);
+//                invalidateOptionsMenu();
+//                clearUI();
+            } else if (BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED.equals(action)) {
+                // Show all the supported services and characteristics on the user interface.
+//                displayGattServices(mBluetoothLeService.getSupportedGattServices());
+            } else if (BluetoothLeService.ACTION_DATA_AVAILABLE.equals(action)) {
+//                displayData(intent.getStringExtra(BluetoothLeService.EXTRA_DATA));
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        EventBus.builder().build();
+
         setContentView(R.layout.activity_main);
     }
 
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        EventBus.getDefault().register(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        unregisterReceiver(mGattUpdateReceiver);
+    }
+
+    @Override
+    public void onStop() {
+        EventBus.getDefault().unregister(this);
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unbindService(mServiceConnection);
+        mBluetoothLeService = null;
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -29,10 +145,63 @@ public class MainActivity extends Activity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_scan) {
+            final Intent intent = new Intent(this, DeviceScanActivity.class);
+            startActivityForResult(intent, 0);
+
             return true;
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (resultCode == RESULT_OK) {
+            mDeviceName = data.getStringExtra(EXTRAS_DEVICE_NAME);
+            mDeviceAddress = data.getStringExtra(EXTRAS_DEVICE_ADDRESS);
+
+            EventBus.getDefault().post(new DeviceSelectedEvent(mDeviceName, mDeviceAddress));
+
+            Log.d(TAG, "mDeviceName: " + mDeviceName + " mDeviceAddress: " + mDeviceAddress);
+        }
+    }
+
+    public void onEvent(MainActivityFragment.JoystickControlEvent event) {
+        Log.d(TAG, "x:" + String.valueOf(event.x) + " y:" + String.valueOf(event.y));
+    }
+
+    private void startBLEConnection() {
+        Intent gattServiceIntent = new Intent(this, BluetoothLeService.class);
+        bindService(gattServiceIntent, mServiceConnection, BIND_AUTO_CREATE);
+
+        registerReceiver(mGattUpdateReceiver, makeGattUpdateIntentFilter());
+        if (mBluetoothLeService != null) {
+            final boolean result = mBluetoothLeService.connect(mDeviceAddress);
+            Log.d(TAG, "Connect request result=" + result);
+        }
+    }
+
+
+
+
+
+    private void updateConnectionState(final int resourceId) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                EventBus.getDefault().post(new DeviceStatusEvent(getResources().getString(resourceId)));
+            }
+        });
+    }
+
+
+    private static IntentFilter makeGattUpdateIntentFilter() {
+        final IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_CONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_DISCONNECTED);
+        intentFilter.addAction(BluetoothLeService.ACTION_GATT_SERVICES_DISCOVERED);
+        intentFilter.addAction(BluetoothLeService.ACTION_DATA_AVAILABLE);
+        return intentFilter;
     }
 }
--- Android/JanBot/app/src/main/java/com/evanli/janbot/MainActivityFragment.java
+++ Android/JanBot/app/src/main/java/com/evanli/janbot/MainActivityFragment.java
@@ -2,14 +2,39 @@ package com.evanli.janbot;
 
 import android.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.zerokol.views.JoystickView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import de.greenrobot.event.EventBus;
+
+import static com.evanli.janbot.MainActivity.*;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+    @Bind(R.id.joystick) JoystickView joystick;
+    @Bind(R.id.status) TextView statusTextView;
+    @Bind(R.id.device) TextView deviceTextView;
+    @Bind(R.id.data) TextView textView;
+
+    public class JoystickControlEvent {
+        public final double x;
+        public final double y;
+
+        public JoystickControlEvent(double x, double y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public MainActivityFragment() {
     }
@@ -17,6 +42,54 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ButterKnife.bind(this, view);
+
+        joystick.setOnJoystickMoveListener(new JoystickView.OnJoystickMoveListener() {
+            @Override
+            public void onValueChanged(int angle, int power, int direction) {
+
+                double x = power * Math.cos(Math.toRadians(angle)) / 100.0f;
+                double y = power * Math.sin(Math.toRadians(angle)) / 100.0f;
+
+                EventBus.getDefault().post(new JoystickControlEvent(x, y));
+            }
+        }, JoystickView.DEFAULT_LOOP_INTERVAL);
+
+        EventBus.getDefault().register(this);
+
+        return view;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+    }
+
+    @Override
+    public void onDestroy() {
+        EventBus.getDefault().unregister(this);
+        super.onDestroy();
+    }
+
+    public void onEvent(MainActivity.DeviceStatusEvent event){
+        statusTextView.setText(event.statusMessage);
+    }
+
+    public void onEvent(MainActivity.DeviceSelectedEvent event){
+
+        String name = "Unknown Device";
+
+        if (event.deviceName != null) {
+            name = event.deviceName;
+        }
+
+        deviceTextView.setText(name + " " + event.deviceAddress);
+
+        Log.d("JanBot", "DeviceSelectedEvent: " + event);
     }
 }
+
+
